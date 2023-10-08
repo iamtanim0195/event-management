49a97ee21cc229bda91f312de39728f63504adf3
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-08 19:09:36 +0600

    about us done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Header/Banner/Banner"
+import AboutUs from "../../components/Section/AboutUs"
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AboutUs></AboutUs>
         </div>
     )
 }
---
+++ src/components/Section/AboutUs.jsx
@@ -0,0 +1,43 @@
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1 className="text-3xl p-5 text-black font-bold text-center">About Us</h1>
+            <div className="flex flex-col gap-5 md:flex-row">
+                <div className="hero bg-base-200">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="text-5xl font-bold">Crafting Unforgettable Birthday Experiences</h1>
+                            <p className="py-6">
+                                we are passionate about making dreams come true and creating magical moments. Our journey began with a simple belief: every child deserves a birthday celebration filled with wonder, joy, and cherished memories.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hero bg-base-200">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="text-5xl font-bold">Our Mission</h1>
+                            <p className="py-6">
+                                We believe that childhood should be celebrated in the most extraordinary way possible. Our mission is to design and execute birthday parties that go beyond expectations, bringing to life the unique dreams and aspirations of every chi
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hero bg-base-200">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="text-5xl font-bold">Our Team</h1>
+                            <p className="py-6">
+                                Behind every successful celebration is a team of dedicated and creative professionals. Our experienced event planners, decorators, entertainers, and coordinators work seamlessly to ensure that every detail of your child's special day is nothing short of perfect.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs;
