fbcd7513e3ede9a03b6c07a1112e4e398782e489
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-08 19:18:30 +0600

    two extra sections done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Header/Banner/Banner"
 import AboutUs from "../../components/Section/AboutUs"
+import ContactUs from "../../components/Section/ContactUs"
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutUs></AboutUs>
+            <ContactUs></ContactUs>
         </div>
     )
 }
